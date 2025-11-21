91be24b20c8e7d084a885012871033ac49ea287d
je1ii <167274389+je1ii@users.noreply.github.com>
2025-11-21 22:15:29 +0800

    added xyz rotation

--- Assets/Scripts/LineGen.cs
+++ Assets/Scripts/LineGen.cs
@@ -18,6 +18,7 @@ public class LineGen : MonoBehaviour
     public float shapeSize;
     public Vector2 shapePos;
     public float zPos;
+    public Vector3 shapeRotation = new Vector3(0, 0, 0);
     
     public ShapeType selectedShape;
     
@@ -33,31 +34,31 @@ public class LineGen : MonoBehaviour
         {
             case ShapeType.Cube:
                 if (cube != null)
-                    cube.DrawCube(material, shapeSize, shapePos, zPos);
+                    cube.DrawCube(material, zPos, shapeSize, shapePos, shapeRotation);
                 else
                     Debug.Log("Reference Missing");
                 break;
             case ShapeType.Pyramid:
                 if (pyramid != null)
-                    pyramid.DrawPyramid(material, shapeSize, shapePos, zPos);
+                    pyramid.DrawPyramid(material, shapeSize, shapePos, zPos, shapeRotation);
                 else
                     Debug.Log("Reference Missing");
                 break;
             case ShapeType.RectangularColumn:
                 if (rectangularColumn != null)
-                    rectangularColumn.DrawRectangularColumn(material, shapeSize, shapePos, zPos);
+                    rectangularColumn.DrawRectangularColumn(material, shapeSize - 1, shapeSize + 3, shapePos, zPos, shapeRotation);
                 else
                     Debug.Log("Reference Missing");
                 break;
             case ShapeType.Sphere:
                 if (sphere != null)
-                    sphere.DrawSphere(material, zPos, shapeSize);
+                    sphere.DrawSphere(material, zPos, shapeSize, shapePos, shapeRotation);
                 else
                     Debug.Log("Reference Missing");
                 break;
             case ShapeType.Cylinder:
                 if (cylinder != null)
-                    cylinder.DrawCylinder(material, zPos, shapeSize - 1, shapeSize + 3);
+                    cylinder.DrawCylinder(material, zPos, shapeSize - 1, shapeSize + 3, shapePos, shapeRotation);
                 else
                     Debug.Log("Reference Missing");
                 break;
